10430696c9769d4407006cd55221da010f8c4f14
M. Brock Podgurski <mbpod10@gmail.com>
2022-04-29 13:35:22 -0400

    create custom hook for request

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,42 +1,31 @@
-import { useState } from 'react';
+// import { useCallback } from 'react';
+import useHttp from '../../hooks/use-http';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp()
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  }
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b69a-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    const newTaskRequest = {
+      url: 'https://react-http-6b69a-default-rtdb.firebaseio.com/tasks.json',
+      method: 'POST',
+      body: { text: taskText },
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }
-    setIsLoading(false);
-  };
+    await sendTaskRequest(newTaskRequest, createTask.bind(null, taskText))
+  }
 
   return (
     <Section>
@@ -44,6 +33,6 @@ const NewTask = (props) => {
       {error && <p>{error}</p>}
     </Section>
   );
-};
+}
 
-export default NewTask;
+export default NewTask
